fbc38d1efae572d84ad8f2453c995772b6b9a65c
Robert Roeser <rroeserr@gmail.com>
2017-02-23 21:18:05 -0800

    byte buddy is fun'

--- agent/src/main/java/example/javaagent/PropertyAgent.java
+++ agent/src/main/java/example/javaagent/PropertyAgent.java
@@ -23,10 +23,14 @@ import javassist.CtField;
 import javassist.NotFoundException;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.description.annotation.AnnotationList;
+import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.bytecode.Throw;
 import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
 import org.reflections.Reflections;
 import org.reflections.scanners.FieldAnnotationsScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
@@ -35,163 +39,55 @@ import org.reflections.util.ConfigurationBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.security.ProtectionDomain;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 
 final class PropertyAgent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("Executing the premain in Agent");
-
-        final Properties properties = new Properties();
-        try (final InputStream stream = PropertyAgent.class.getClassLoader().getResourceAsStream("app.properties")) {
-            properties.load(stream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        final Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forPackage("example.javaagent"))
-                .setScanners(new TypeAnnotationsScanner(),
-                        new FieldAnnotationsScanner()));
-
-        final Map<Class<?>, Set<Field>> fieldMap = new HashMap<>();
-        final Set<Field> fields = reflections.getFieldsAnnotatedWith(Property.class);
-
-        fields.forEach(f -> {
-            Class<?> clazz = f.getDeclaringClass();
-            if (!fieldMap.containsKey(clazz)) {
-                Set<Field> clazzFields = new HashSet<>();
-                fields.add(f);
-                fieldMap.put(clazz, clazzFields);
-            } else {
-                fieldMap.get(clazz).add(f);
-            }
-        });
-        
-        try {
+       
+       new AgentBuilder.Default()
+                .type(new AgentBuilder.RawMatcher() {
+                    @Override
+                    public boolean matches(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, Class<?> classBeingRedefined, ProtectionDomain protectionDomain) {
     
-            fieldMap
-                .entrySet()
-                .stream()
-                .flatMap(entry -> {
-                    final Class<?> clazz = entry.getKey();
-                    final Set<Field> value = entry.getValue();
-            
-                    return value
-                               .stream()
-                               .map(field -> {
-                        System.out.println("class " + clazz.getName() + " - field - " + field.getName());
+                        return typeDescription
+                            .getDeclaredFields()
+                            .parallelStream()
+                            .anyMatch(inDefinedShape ->
+                                          inDefinedShape
+                                              .getDeclaredAnnotations()
+                                              .parallelStream()
+                                              .map(AnnotationDescription::getAnnotationType)
+                                              .anyMatch(typeDefinitions -> typeDefinitions.isAssignableTo(Property.class))
+                            );
                         
-                        //return field;*/
-                    
-                                   Property annotation = field
-                                                             .getAnnotation(Property.class);
-                                   Object o = properties
-                                                  .get(annotation.value());
-                    
-                    
-                                   Class<?> type = field
-                                                       .getType();
-                    
-                                   new AgentBuilder
-                                           .Default()
-                                       .type(ElementMatchers.named(clazz.getName()))
-                                       .transform((builder, typeDescription, classLoader, module) -> {
-                                           DynamicType.Builder.FieldDefinition.Valuable<?> field1 = builder
-                                                                                                        .field(ElementMatchers.is(field));
-                            
-                                           if (type.isAssignableFrom(Integer.class)) {
-                                               field1.value((int) o);
-                                           } else if (type.isAssignableFrom(Long.class)) {
-                                               field1.value((long) o);
-                                           } else if (type.isAssignableFrom(Double.class)) {
-                                               field1.value((double) o);
-                                           } else if (type.isAssignableFrom(Boolean.class)) {
-                                               field1.value((boolean) o);
-                                           } else if (type.isAssignableFrom(Float.class)) {
-                                               field1.value((float) o);
-                                           } else if (type.isAssignableFrom(String.class)) {
-                                               field1.value((String) o);
-                                           }
-                            
-                                           return builder;
-                                       })
-                                       .installOn(inst);
-                    
-                    
-                                   return field;
-                               });
-            
-                })
-                .collect(Collectors.toList());
+    /*
     
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-        
-        
-        /*
-        ClassReloadingStrategy classReloadingStrategy = ClassReloadingStrategy
-                                                   .fromInstalledAgent();
-new ByteBuddy()
-  .redefine(Foo.class)
-  .method(named("getHello"))
-  .intercept(FixedValue.value("Byte Buddy!"))
-  .make()
-  .load(Foo.class.getClassLoader(), classReloadingStrategy);
-assertThat(foo.getHello(), is("Byte Buddy!"));
-classReloadingStrategy.reset(Foo.class);
-assertThat(foo.getHello(), is("Hello World"));
-        
-        
-        new AgentBuilder.Default()
-                .type(ElementMatchers.any())
-                .transform((builder, typeDescription, classLoader, module) -> {
-                    return builder.field(ElementMatchers.anyOf(fields))
-                            .transform((instrumentedType, target) -> {
-                                System.out.println(target);
-                                return null;
+                        Arrays
+                            .asList(classBeingRedefined.getFields())
+                            .stream()
+                            .filter(field -> field.getAnnotation(Property.class) != null)
+                            .forEach(field -> {
+                                System.out.println("fuck you byte buddy -> " + field.getName());
                             });
+     */
+    
+                    }
+                })
+                .transform((builder, typeDescription, classLoader, module) -> {
+                    System.out.println("SLAYER");
+                    return builder;
                 })
                 .installOn(inst);
-         */
         
-        /*
-        
-        
-        inst.addTransformer((classLoader, s, clazz, protectionDomain, bytes) -> {
-            if (fieldMap.containsKey(clazz)) {
-                try {
-                    ClassPool cp = ClassPool.getDefault();
-                    CtClass cc = cp.get(clazz.getName());
-
-                    fieldMap.get(clazz).forEach(f -> {
-                        try {
-                            CtField ctField = cc.getField(f.getName());
-                        } catch (NotFoundException e) {
-                            throw new RuntimeException("Error load-time weaving properties for class '" + clazz.getName() + "'");
-                        }
-                    });
-
-                    byte[] byteCode = cc.toBytecode();
-                    cc.detach();
-                    return byteCode;
-                } catch (Exception e) {
-                    throw new RuntimeException("Error load-time weaving properties for class '" + clazz.getName() + "'");
-                }
-            }
-
-            return null;
-        });*/
-        
-
         System.out.println("Agent premain executed!");
     }
 
